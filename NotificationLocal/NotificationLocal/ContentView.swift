7ff4965dd399366b0a9fb2eb7bd2039c41ceab35
chanoktrue <chanoktrue@me.com>
2021-09-02 05:23:17 +0700

    complete

--- NotificationLocal/NotificationLocal/ContentView.swift
+++ NotificationLocal/NotificationLocal/ContentView.swift
@@ -15,26 +15,42 @@ struct ContentView: View {
             Button("Request permission") {
                 NotificationManger.shared.requestAuthoriztion()
             }
+            .frame(width: UIScreen.main.bounds.width * 0.8, height: 50)
+            .background(Color.green.opacity(0.5))
+            .cornerRadius(8)
             .padding()
             
             Button("Schedule notification Time") {
                 NotificationManger.shared.scheduleNotificationTime()
             }
+            .frame(width: UIScreen.main.bounds.width * 0.8, height: 50)
+            .background(Color.green.opacity(0.5))
+            .cornerRadius(8)
             .padding()
             
             Button("Schedule notification Calendar") {
                 NotificationManger.shared.scheduleNotificationCalendar()
             }
+            .frame(width: UIScreen.main.bounds.width * 0.8, height: 50)
+            .background(Color.green.opacity(0.5))
+            .cornerRadius(8)
             .padding()
             
             Button("Schedule notification Location") {
                 NotificationManger.shared.scheduleNotificationLocation()
             }
+            .frame(width: UIScreen.main.bounds.width * 0.8, height: 50)
+            .background(Color.green.opacity(0.5))
+            .cornerRadius(8)
             .padding()
             
             Button("Cancel notification") {
                 NotificationManger.shared.cancelNotification()
             }
+            .frame(width: UIScreen.main.bounds.width * 0.8, height: 50)
+            .background(Color.green.opacity(0.5))
+            .cornerRadius(8)
+            .padding()
             
         }
         .onAppear{
